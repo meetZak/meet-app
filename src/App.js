b74b0e88aaafcc0325043fa55a8d09b8c53db771
abuyusr <abdizm@outlook.com>
2023-06-06 18:08:08 -0400

    Created test folder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from "./NumberOfEvents";
+import { getEvents, extractLocations } from './api';
+import './nprogress.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    selectedLocation: 'all',
+    numberOfEvents: 32
+  };
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+      this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
+  updateNumberOfEvents(number) { 
+    this.setState({
+      numberOfEvents: number,
+    })
+  }
+
+  updateEvents = (location, inputNumber) => {
+    const { selectedLocation } = this.state;
+    if (location) {
+      getEvents().then((events) => {
+        const locationEvents = (location === 'all') ?
+          events :
+          events.filter((event) => event.location === location);
+        const eventsToShow = locationEvents.slice(0, inputNumber);
+        this.setState({
+          events: eventsToShow,
+          selectedLocation: location,
+          numberOfEvents: inputNumber
+        });
+      });
+    } else {
+      getEvents().then((events) => {
+        const locationEvents = (selectedLocation === 'all') ?
+          events :
+          events.filter((event) => event.location === selectedLocation);
+        const eventsToShow = locationEvents.slice(0, inputNumber);
+        this.setState({
+          events: eventsToShow,
+          numberOfEvents: inputNumber 
+        });
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CitySearch  locations={this.state.locations} updateEvents={this.updateEvents} />
+        <EventList events={this.state.events} />
+        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
+      </div>
+    );
+  }
 }
 
 export default App;
