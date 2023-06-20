b0ac2d3db8fdfdb0828c1b163d8abcf8cfe0abdc
abuyusr <abdizm@outlook.com>
2023-06-20 18:23:43 -0400

    Created Alert file

---
+++ src/Alert.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Alert extends Component {
+  constructor(props) {
+    super(props);
+    this.color = null; 
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+      textAlign: 'center'
+     
+    };
+  }
+
+  render() {
+    return (
+      <div className="Alert" style={{ textAlign: 'center' }}>
+        <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+}
+
+class InfoAlert extends Alert {
+    constructor(props) {
+      super(props);
+      this.color = 'blue';
+      
+    }
+  }
+
+class ErrorAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = 'red';
+    }
+}
+
+class WarningAlert extends Alert { 
+    constructor(props) {
+        super(props);
+        this.color = 'orange';
+    }
+}
+
+  export { InfoAlert, ErrorAlert, WarningAlert };
