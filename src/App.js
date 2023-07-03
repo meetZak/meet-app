ce649394ca790cb5ab5276b503c11893df87c98e
abuyusr <abdizm@outlook.com>
2023-07-03 17:04:45 -0400

    Added Scatter Chart

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
-import { getEvents, extractLocations } from './api';
+import WelcomeScreen from "./WelcomeScreen";
+import { getEvents, extractLocations, checkToken, getAccessToken } from "./api"; 
 import NumberOfEvents from "./NumberOfEvents";
 import "./nprogress.css";
 import { WarningAlert } from './Alert';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
+} from 'recharts';
 
 
 
@@ -16,19 +20,27 @@ class App extends Component {
     eventCount: 32,
     selectedCity: null,
     warningText: "",
+    showWelcomeScreen: undefined,
   }
-
-  componentDidMount() {
+  async componentDidMount(){
     this.mounted = true;
-    this.promptOfflineWarning();
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
     getEvents().then((events) => {
       if (this.mounted) {
-      this.setState({ events, locations: extractLocations(events) });
-      }
+      this.setState({ 
+        events: events,
+        locations: extractLocations(events)
+      });
+    }
     });
   }
-
-  componentWillUnmount(){
+  }
+  componentWillUnmount() {
     this.mounted = false;
   }
 
@@ -69,16 +81,51 @@ class App extends Component {
     }
   }
 
+  getData = () => {
+    const {locations, 
+  events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
   render() {
+    const { locations, numberOfEvents } = this.state;
     return (
       <div className="App">
-        <CitySearch  locations={this.state.locations} updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
-        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
+        <CitySearch updateEvents={this.updateEvents} locations={locations} />
+        <NumberOfEvents
+          numberOfEvents={numberOfEvents}
+        />       
+         <h4>Events in each city</h4>
+
+         <ScatterChart
+          width={400}
+          height={400}
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis type="number" dataKey="number" name="number of events" unit="kg" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={this.getData} fill="#8884d8" />
+        </ScatterChart>
         <WarningAlert text={this.state.warningText} />
+        <EventList events={this.state.events} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} 
+        getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
-}
+  };
+
+
 
 export default App;
